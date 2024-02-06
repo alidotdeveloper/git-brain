ac48c3cc4b70216fb460f85721fb10e1867630f4
Ali <alihassnain330@gmail.com>
2024-02-06 22:46:34 +0500

    gitbrain project setup and header is already setup

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./Components/Header";
+
+function App() {
+
+
+  return (
+    <>
+      <Header />
+     <h1 className=" text-red-500">
+      Hello world!
+    </h1>
+    
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Header() {
+    return (
+    <div className='text-center bg-red-500'>Header</div>
+  )
+}
+
+export default Header
