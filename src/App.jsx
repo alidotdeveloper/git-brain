f9b5af7d846916b136e21fd153ad6dc0cc0dd84b
Ali <alihassnain330@gmail.com>
2024-02-11 22:38:46 +0500

    pros done for RIsection

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,17 @@ function App() {
       <Header />
       <Main />
       <HeadingImage />
-      <RIsection />
+      <RIsection
+        gradcolor=" bg-gradient-to-r from-teal-200 to-[#5b7ba8]"
+        gradhead="Split code changes"
+        whitetext="into"
+        whitesmall= "multiple AI-generated commits"
+        summary="  As developers, we often forget to commit our changes, and end up with a huge commit that is hard to review.
+        GitBrain intelligently splits your code changes into multiple AI-generated commits and even allows you
+        to interactively edit them. You can select, reorder, and even merge multiple commits into one with the help of our AI."
+        src="../../images/ai-commit.gif"
+      
+      />
       <LIsection
         img="https://gitbrain.dev/_next/image?url=%2Fimages%2Fsummary.png&w=1920&q=75"
         gradhead="Self code review"
