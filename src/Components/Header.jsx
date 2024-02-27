37757d6f8644fb66455a465f07f88253cd476976
Ali <alihassnain330@gmail.com>
2024-02-27 22:16:53 +0500

    fix header in mobile responsiveness

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,19 +3,21 @@ import React from 'react'
 function Header() {
 
     return (
-        <header class="text-gray-600 body-font">
-        <div class="container mx-auto flex flex-wrap p-5 pl-28 pr-28 flex-col md:flex-row items-center ">
+        <header class="text-gray-600 body-font ">
+        <div class="container mx-auto flex flex-wrap p-5 pl-3 pr-3  md:flex-row   items-center ">
+       
           <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 bg-transparent">
           <img src="../../images/gitbrain.png" alt= "image-logo" width={28} height={28}  />   
             <span class="ml-3 text-xl text-white">GitBrain</span>
-          </a>
+            </a>
+        
           <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
             {/* <a class="mr-5 hover:text-white-900">First Link</a>
             <a class="mr-5 hover:text-white-900">Second Link</a>
             <a class="mr-5 hover:text-white-900">Third Link</a>
             <a class="mr-5 hover:text-white-900">Fourth Link</a> */}
           </nav>
-          <button class="rounded bg-green-950 text-gray-400 font-medium inline-flex items-center  border-0 py-1 px-3 focus:outline-none hover:bg-#173D43,text-white mt-4 md:mt-0">Download
+          <button class="rounded bg-green-950 text-gray-400 font-medium inline-flex items-center hidden  border-0 py-1 px-3 focus:outline-none hover:bg-{#173D43} text-white mt-2 md:mt-0 ">Download
           </button>
         </div>
       </header>
